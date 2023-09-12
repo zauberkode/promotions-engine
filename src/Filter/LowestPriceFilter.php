34ba1919ad6e8a9e8d8620577b6b64a0b5548626
AK <ndrkrpv@gmail.com>
2023-09-12 09:00:59 +0300

    Add filter

---
+++ src/Filter/LowestPriceFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Filter;
+
+
+use App\DTO\PromotionEnquiryInterface;
+
+class LowestPriceFilter implements PromotionsFilterInterface
+{
+
+    public function apply(PromotionEnquiryInterface $enquiry): PromotionEnquiryInterface
+    {
+        $enquiry->setDiscountedPrice(50);
+        $enquiry->setPrice(100);
+        $enquiry->setPromotionId(3);
+        $enquiry->setPromotionName('Black Friday Half Price Sale');
+
+        return $enquiry;
+    }
+}
