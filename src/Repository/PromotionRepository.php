fde8c8a1f242b674a6839f7c44c35de4e519e3a3
AK <ndrkrpv@gmail.com>
2023-09-14 10:33:49 +0300

    Add custom method into repository

--- src/Repository/PromotionRepository.php
+++ src/Repository/PromotionRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Product;
 use App\Entity\Promotion;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -20,6 +21,17 @@ class PromotionRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Promotion::class);
     }
+
+    public function findValidForProduct(Product $product, \DateTimeInterface $requestDate) {
+        return $this->createQueryBuilder('p')->innerJoin('p.productPromotions', 'pp')
+            ->andWhere('pp.product = :product')
+            ->andWhere('pp.validTo > :requestDate OR pp.validTo IS NULL')
+            ->setParameter('product', $product)
+            ->setParameter('requestDate', $requestDate)
+            ->getQuery()
+            ->getResult();
+    }
+
 
 //    /**
 //     * @return Promotion[] Returns an array of Promotion objects
