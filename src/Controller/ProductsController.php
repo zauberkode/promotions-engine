b4b53e0824c997117e2c264769a0a87683d29c8f
AK <ndrkrpv@gmail.com>
2023-09-14 10:33:07 +0300

    Make filter work

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Entity\Promotion;
 use App\Filter\PromotionsFilterInterface;
+use App\Repository\ProductRepository;
 use App\Service\Serializer\DTOSerializer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
+
+    public function __construct(
+        private ProductRepository $repository,
+        private EntityManagerInterface $entityManager
+    )
+    {
+    }
 
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
     public function lowestPrice(
@@ -27,9 +37,18 @@ class ProductsController extends AbstractController
                 'error' => 'Promotions engine failure message',
             ], 400);
         }
-
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
         $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
-        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry);
+
+        $product = $this->repository->find($id); //Add error handling for not found product
+        $lowestPriceEnquiry->setProduct($product);
+
+        $promotions = $this->entityManager->getRepository(Promotion::class)->findValidForProduct(
+            $product,
+            date_create_immutable($lowestPriceEnquiry->getRequestDate())
+        );
+
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry, $promotions);
         $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
 
         return new Response($responseContent, 200);
