3ad7192edcfc3b0dd2ff006e9b01739135ca228d
AK <ndrkrpv@gmail.com>
2023-10-10 19:13:01 +0300

    Add even items multiplier modifier

--- tests/unit/PriceModifiersTest.php
+++ tests/unit/PriceModifiersTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\EvenItemsMultiplier;
 use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 
@@ -14,6 +15,7 @@ class PriceModifiersTest extends ServiceTestCase
     /** @test */
    public function DateRangeMultiplier_returns_a_correctly_modified_price(): void
    {
+       // Given
        $enquiry = new LowestPriceEnquiry();
        $enquiry->setQuantity(5);
        $enquiry->setRequestDate('2022-11-27');
@@ -24,7 +26,6 @@ class PriceModifiersTest extends ServiceTestCase
        $promotion->setCriteria(["from" => "2022-11-25", "to" => "2022-11-28"]);
        $promotion->setType('date_range_multiplier');
 
-       // Given
        $dateRangeModifier = new DateRangeMultiplier();
        // When
        $modifiedPrice = $dateRangeModifier->modify(100, 5, $promotion, $enquiry);
@@ -38,6 +39,7 @@ class PriceModifiersTest extends ServiceTestCase
     /** @test */
     public function FixedPriceVoucher_returns_a_correctly_modified_price(): void
     {
+        // Given
         $enquiry = new LowestPriceEnquiry();
         $enquiry->setQuantity(5);
         $enquiry->setVoucherCode('OU812');
@@ -48,7 +50,6 @@ class PriceModifiersTest extends ServiceTestCase
         $promotion->setCriteria(["code" => "OU812"]);
         $promotion->setType('fixed_price_voucher');
 
-        // Given
         $fixedPriceVoucher = new FixedPriceVoucher();
         // When
         $modifiedPrice = $fixedPriceVoucher->modify(150, 5, $promotion, $enquiry);
@@ -57,4 +58,49 @@ class PriceModifiersTest extends ServiceTestCase
         $this->assertSame(500, $modifiedPrice);
 
     }
+
+    /** @test */
+    public function EvenItemsMultiplier_returns_a_correctly_modified_price(): void
+    {
+
+        // Given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one free');
+        $promotion->setAdjustment(0.5);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+        // When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+
+        // Then
+        $this->assertSame(300, $modifiedPrice);
+    }
+
+    /** @test */
+    public function EvenItemsMultiplier_correctly_calculates_alternatives(): void
+    {
+
+        // Given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one free');
+        $promotion->setAdjustment(0.75);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+        // When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+
+        // Then
+        $this->assertSame(400, $modifiedPrice);
+    }
+
 }
