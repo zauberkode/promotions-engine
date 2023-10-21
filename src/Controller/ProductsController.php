38e9dc0cf6de8d91d699063b6fbdd51fd66da954
AK <ndrkrpv@gmail.com>
2023-10-21 10:06:54 +0300

    Add the cache service

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Cache\PromotionsCache;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\PromotionsFilterInterface;
@@ -13,13 +14,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ProductsController extends AbstractController
 {
 
     public function __construct(
         private ProductRepository $repository,
-        private EntityManagerInterface $entityManager
     )
     {
     }
@@ -29,7 +31,8 @@ class ProductsController extends AbstractController
         Request $request,
         int $id,
         DTOSerializer $serializer,
-        PromotionsFilterInterface $promotionsFilter
+        PromotionsFilterInterface $promotionsFilter,
+        PromotionsCache $promotionsCache
     ): Response
     {
         if ($request->headers->has('force_fail')) {
@@ -43,10 +46,8 @@ class ProductsController extends AbstractController
         $product = $this->repository->find($id); //Add error handling for not found product
         $lowestPriceEnquiry->setProduct($product);
 
-        $promotions = $this->entityManager->getRepository(Promotion::class)->findValidForProduct(
-            $product,
-            date_create_immutable($lowestPriceEnquiry->getRequestDate())
-        );
+        $promotions = $promotionsCache->findValidForProduct($product, $lowestPriceEnquiry->getRequestDate());
+
 
         $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry, ...$promotions);
         $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
