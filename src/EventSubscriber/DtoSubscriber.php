85a0069b2a5f946219519768a0800863c3a4c86c
AK <ndrkrpv@gmail.com>
2023-10-24 09:17:38 +0300

    Add validation and test

--- src/EventSubscriber/DtoSubscriber.php
+++ src/EventSubscriber/DtoSubscriber.php
@@ -6,18 +6,32 @@ namespace App\EventSubscriber;
 
 use App\Event\AfterDtoCreatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DtoSubscriber implements EventSubscriberInterface
 {
 
-    public static function getSubscribedEvents()
+    /**
+     * DtoSubscriber constructor.
+     */
+    public function __construct(private ValidatorInterface $validator)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
     {
         return [
-            AfterDtoCreatedEvent::NAME => 'validateDto',
+             AfterDtoCreatedEvent::NAME =>
+                ['validateDto', 100]
         ];
     }
 
     public function validateDto(AfterDtoCreatedEvent $event):void {
-
+        $dto = $event->getDto();
+        $errors = $this->validator->validate($dto);
+        if (count($errors) > 0) {
+            throw new ValidationFailedException('Validation Falied', $errors);
+        }
     }
 }
--- src/Service/Serializer/DTOSerializer.php
+++ src/Service/Serializer/DTOSerializer.php
@@ -4,7 +4,9 @@
 namespace App\Service\Serializer;
 
 
+use App\Event\AfterDtoCreatedEvent;
 use Doctrine\Common\Annotations\AnnotationReader;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
@@ -17,8 +19,10 @@ class DTOSerializer implements SerializerInterface
 {
 
     private SerializerInterface $serializer;
+    private EventDispatcherInterface $eventDispatcher;
 
-    public function __construct() {
+    public function __construct(EventDispatcherInterface $eventDispatcher) {
+        $this->eventDispatcher = $eventDispatcher;
         $this->serializer = new Serializer(
             [new ObjectNormalizer(
                 classMetadataFactory: new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader())),
@@ -36,6 +40,8 @@ class DTOSerializer implements SerializerInterface
     public function deserialize(mixed $data, string $type, string $format, array $context = []): mixed
     {
         $dto = $this->serializer->deserialize($data, $type, $format, $context);
+        $event = new AfterDtoCreatedEvent($dto);
+        $this->eventDispatcher->dispatch($event, $event::NAME);
 
         return $dto;
     }
